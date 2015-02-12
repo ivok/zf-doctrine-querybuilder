bcfbbc2a3de3b8875590c5a5ff8d43a529f56622
i.kuzov <i.kuzov@viscomp.bg>
2015-02-12 09:00:37 +0200

    fixed ORMOrderByManager

--- src/OrderBy/Service/ORMOrderByManager.php
+++ src/OrderBy/Service/ORMOrderByManager.php
@@ -26,7 +26,7 @@ class ORMOrderByManager extends AbstractPluginManager
 
             $orderByHandler = $this->get(strtolower($option['type']), array($this));
 
-            $orderByHandler->oddOrderBy($queryBuilder, $metadata, $option);
+            $orderByHandler->addOrderBy($queryBuilder, $metadata, $option);
         }
     }
 
